89031447cd54f70d6c3b2ce84c67775a793b4806
Justin Glommen <jglommen@gmail.com>
2017-04-19 18:25:16 -0700

    Threaded IntentService

--- app/src/main/java/com/android/servicetest/DemoService.java
+++ app/src/main/java/com/android/servicetest/DemoService.java
@@ -1,55 +1,98 @@
 package com.android.servicetest;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
-import android.widget.EditText;
+import android.content.Context;
 import android.widget.Toast;
 
-public class DemoService extends Service {
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * helper methods.
+ */
+public class DemoService extends IntentService {
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "lab3.lab3.com.intentservice.action.FOO";
+    private static final String ACTION_BAZ = "lab3.lab3.com.intentservice.action.BAZ";
+
+    private static final String EXTRA_PARAM1 = "lab3.lab3.com.intentservice.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "lab3.lab3.com.intentservice.extra.PARAM2";
+
     public DemoService() {
+        super("worker");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, DemoService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, DemoService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            synchronized (this) {
+                try {
+                    wait(15000);
+                }
+                catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            stopService(intent);
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(DemoService.this, "Service Started", Toast.LENGTH_SHORT).show();
-        Thread thread = new Thread(new myThread(startId));
-        thread.start();
         return super.onStartCommand(intent, flags, startId);
+
     }
 
+    @Override
     public void onDestroy() {
         Toast.makeText(DemoService.this, "Service Stopped", Toast.LENGTH_SHORT).show();
         super.onDestroy();
-    }
-
-
-    final class myThread implements Runnable {
-        int startId;
-
-        public myThread(int startId) {
-            this.startId = startId;
-        }
-
-        @Override
-        public void run() {
-            synchronized (this) {
-                try {
-                    wait(15000); // Milliseconds
-                }
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                stopSelf(startId);
-            }
-        }
 
     }
-
 }
